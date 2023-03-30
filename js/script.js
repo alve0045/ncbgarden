19a6d98580ab5a235834d1e72bc1235c095d06a0
oshandi <piyumioshandi@gmail.com>
2023-03-30 14:41:40 -0400

    Updated working script file

--- js/script.js
+++ js/script.js
@@ -1,10 +1,6 @@
-
-
-// Example starter JavaScript for disabling form submissions if there are invalid fields
 (function () {
-  'use strict'
+  'use strict';
 
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
   var forms = document.querySelectorAll('.needs-validation');
 
   const emailInput = document.querySelector('#email');
@@ -12,60 +8,60 @@
   const lnameInput = document.querySelector('#last-name');
   const messageTextarea = document.querySelector('#message');
 
-  //Removing whitespace
-  const fname = fnameInput.value.trim();
-  const lname = lnameInput.value.trim();
-  const message = messageTextarea.value.trim();
+  // Move the isEmailValid variable declaration here
+  let isEmailValid = false; 
 
-  // Add an event listener to the email input to validate email
-  emailInput.addEventListener('input', function() {
-    const email = emailInput.value.trim();
-    // Regular expression pattern to validate the email
-    const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    // Test the input value against the pattern
-    const isEmailValid = pattern.test(email);
-    // If the email is valid, remove the "is-invalid" class and add the "is-valid" class
-    if (isEmailValid) {
-      emailInput.classList.remove('is-invalid');
-      emailInput.classList.add('is-valid');
-    } else {
-      // If the email is not valid, remove the "is-valid" class and add the "is-invalid" class
-      emailInput.classList.remove('is-valid');
-      emailInput.classList.add('is-invalid');
-    }
+  emailInput.addEventListener('input', function () {
+      const email = emailInput.value.trim();
+      const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      isEmailValid = pattern.test(email);
+      if (isEmailValid) {
+          emailInput.classList.remove('is-invalid');
+          emailInput.classList.add('is-valid');
+      } else {
+          emailInput.classList.remove('is-valid');
+          emailInput.classList.add('is-invalid');
+      }
   });
 
-  // Loop over them and prevent submission
-  Array.prototype.slice.call(forms)
-  .forEach(function (form) {
-    form.addEventListener('submit', function (event) {
-      
-      if (!form.checkValidity() && !isEmailValid) {
-        event.preventDefault()
-        event.stopPropagation()
-      } else {
+  Array.prototype.slice.call(forms).forEach(function (form) {
+      form.addEventListener(
+          'submit',
+          function (event) {
+              // Move the declarations and initializations inside the form submit event listener
+              const fname = fnameInput.value.trim();
+              const lname = lnameInput.value.trim();
+              const message = messageTextarea.value.trim();
+              if (!form.checkValidity() || !isEmailValid) {
+                  event.preventDefault();
+                  event.stopPropagation();
+              } else {
+                // Prevent the default form submission behavior
+                  event.preventDefault(); 
+                  form.classList.add('was-validated');
 
-        form.classList.add('was-validated');
+                  // Construct the email
+                  let body =
+                      'First Name: ' +
+                      fname +
+                      '<br/>Last Name: ' +
+                      lname +
+                      '<br/>Email: ' +
+                      emailInput.value.trim() +
+                      '<br/>Message: ' +
+                      message;
 
-        //Construct the email
-        let body = "First Name: " + formData.fname + "<br/>Last Name: " + formData.lname + "<br/>Email: " + formData.email + 
-                "<br/>Message: " + formData.message;
-
-        //SMTP JS Email Verification
-        Email.send({
-
-            SecureToken : "54ed28c8-faa9-4001-ba7a-d475870aaa9d",
-            To : 'diss0005@algonquinlive.com',
-            From : 'odinquiries@gmail.com',
-            Subject : "You got a new feedback from NCB Microsite",
-            Body : body
-        }).then(
-          message => alert("We appreciate your feedback!")
-        );
-      }
-
-    }, false)
-  })
-
+                  // SMTP JS Email Verification
+                  Email.send({
+                      SecureToken: '54ed28c8-faa9-4001-ba7a-d475870aaa9d',
+                      To: 'diss0005@algonquinlive.com',
+                      From: 'odinquiries@gmail.com',
+                      Subject: 'You got a new feedback from NCB Microsite',
+                      Body: body,
+                  }).then((message) => alert('We appreciate your feedback!'));
+              }
+          },
+          false
+      );
+  });
 })();
-
